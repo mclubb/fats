10b298fd7c1873b89594272b5cf5d417736901f3
Mike Clubb <michael.clubb@sprydigital.com>
2019-02-27 17:34:46 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var express_session = require('express-session');
+var body_parser = require('body-parser');
+var app = express();
+var port = 8088;
+
+app.set('view engine', 'pug');
+
+app.get('/register', function(req, res) {
+    res.render('register');
+})
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+// Start the server
+app.listen(port, function() {
+    console.log("Application Started: \n listening on port: " + port )
+});
