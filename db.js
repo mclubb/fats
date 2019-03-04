8b0230f9b31b9d1384a01b05147063b82a90daf7
Mike Clubb <michael.clubb@sprydigital.com>
2019-03-04 12:21:55 -0600

    Changed the db connection to use a config file

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql')
   , async = require('async')
+  , config = require('config')
 
 var PRODUCTION_DB = 'app_prod_database'
   , TEST_DB = 'app_test_database'
@@ -15,8 +16,8 @@ var state = {
 exports.connect = function(mode, done) {
   state.pool = mysql.createPool({
     host: 'fats.stlwd.com',
-    user: 'fats_user',
-    password: 'fats_secret',
+    user: config.username,
+    password: config.password,
     database: 'fats'
   })
 
